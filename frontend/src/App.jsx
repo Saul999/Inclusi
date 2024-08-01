78ed5e98bf5fe5737827f2bf09160ee728c97659
Saul385 <84159357+Saul999@users.noreply.github.com>
2024-07-31 23:03:50 -0700

    fixed error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import NavBar from "./components/Navbar";
+import NavBar from "./components/NavBar";
 import Cards from "./components/Card";
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
