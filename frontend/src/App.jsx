ac0eaadc4383ff84e6a9e270c5005106b5b8b22a
Saul385 <84159357+Saul999@users.noreply.github.com>
2024-08-02 15:12:36 -0700

    add routes for each page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,18 +2,29 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "./components/NavBar";
 import Cards from "./components/Card";
+import CivicTools from "./components/CivicTools";
 import "./App.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Translator from "./components/Translator";
+import Contact from "./components/Contact";
+import Resources from "./components/Resources";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <NavBar />
-        <Cards />
-        {/* You can add your Routes here */}
+    <div className="App">
+      <NavBar />
+      <div className="page">
+        <Router>
+          <Routes>
+            <Route path="/" element={<Cards></Cards>}></Route>
+            <Route path="/civic-tools" element={<CivicTools />}></Route>
+            <Route path="/translator" element={<Translator />}></Route>
+            <Route path="/resources" element={<Resources />}></Route>
+            <Route path="/contact" element={<Contact />}></Route>
+          </Routes>
+        </Router>
       </div>
-    </Router>
+    </div>
   );
 }
 
